6f5e0d261708b2d1c5bf56ed905e847c9dac5e48
eriknord20 <eriknord@PostNord.local>
2018-09-25 15:13:09 +0200

    fucked up shit

---
+++ film/src/views/button.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+    .button
+        input(type="text" v-model="inputField")
+        button(@click="action") {{buttonText}}
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+        props: ["buttonText"],
+
+        data: () => ({
+            inputField: ""
+        }),
+        methods: {
+            action(params:any) {
+                this.$emit("post_data", this.inputField)
+            }
+        }
+})
+</script>
+<style lang="sass" scoped>
+
+</style>
--- film/src/views/throttler.vue
+++ film/src/views/throttler.vue
@@ -1,14 +1,22 @@
 <template lang="pug">
-    button(@click="throttledMethod()") Click me as fast as you can!
+    searchButton(@post_data="test()" buttonText ="click me")
 </template>
 <script lang="ts">
 import Vue from "vue";
 import _ from "lodash";
+import Button from './button.vue'
 export default Vue.extend({
   methods: {
-    throttledMethod: _.throttle(() => {
-      console.log("I only get fired once every two seconds, max!")
-    }, 2000)
+      components: {
+        'searchButton':Button  
+      },
+    // throttledMethod: _.throttle(() => {
+    //   console.log("I only get fired once every two seconds, max!")
+    // }, 2000),
+
+    test(params:any) {
+        console.log(params)
+    }
   }
 });
 </script>
