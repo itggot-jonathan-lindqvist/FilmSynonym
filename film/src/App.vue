b940ba1a2be5d4349dba1e77915169b62b1c56a7
itggot-simon-johannesson <simon.johannesson2@elev.ga.ntig.se>
2018-09-26 10:40:43 +0200

    added google icos and lobster

--- film/src/App.vue
+++ film/src/App.vue
@@ -8,3 +8,32 @@
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+
+<style lang="sass">
+  .material-icons
+    font-family: 'Material Icons'
+    font-weight: normal
+    font-style: normal
+    font-size: 100px
+    display: inline-block
+    line-height: 1
+    text-transform: none
+    letter-spacing: normal
+    word-wrap: normal
+    white-space: nowrap
+    direction: ltr
+    -webkit-font-smoothing: antialiased
+    text-rendering: optimizeLegibility
+    -moz-osx-font-smoothing: grayscale
+    font-feature-settings: 'liga'
+    
+</style>
+
