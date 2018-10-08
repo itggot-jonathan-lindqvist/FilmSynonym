222bd09dbb184b1e3d789c607a12b93075a9c4ff
Bijan.fatha <bijanfatha@Bijan-fatha.local>
2018-10-08 10:52:14 +0200

    gjorde quiz fil

---
+++ film/src/views/quiz.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+export default Vue.extend({
+    
+})
+</script>
+
+
+<style lang="sass">
+
+</style>
+
