c4ec80e1b87a88cb14c4344ec635bc8c72e3892d
Bijan.fatha <bijanfatha@Bijan-fatha.local>
2018-10-08 15:42:51 +0200

    someshit

--- film/src/views/quiz.vue
+++ film/src/views/quiz.vue
@@ -1,30 +1,60 @@
 <template lang="pug">
     .container-fluid
-        h2 Quiz
+        h2.text-center.display-3 Quiz
         div(v-if="start")
-            .btn-primary.btn-lg.btn.btn-block(@click="start=false" id="btn" ) START
+            .btn-primary.btn-lg.btn.btn-block(@click="start=false, getmovie()" type="button") START
         div(v-else)
-            .title(class="border border-dark")
-                p HEllO
-            .plot
-             p SAy WHAT
+            ul.list-group.shadow-lg.p-3.mb-5.bg-white.rounded()
+                li.list-group-item(v-for="movie in movieIds") {{movie}}
+            .plot.card.shadow-lg.bg-white.rounded.p-3.m-5
+                .card-body
+                    p.card-text.text-center.lead Hello ma friend
+
 
 </template>
 <script lang="ts">
 import Vue from "vue";
 import axios from "axios";
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css'
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
 
 Vue.use(BootstrapVue);
+let key = "57b31362",
+  baseurl = "https://www.omdbapi.com/?apikey=" + key;
 export default Vue.extend({
   data: function() {
     return {
-      start: true
+      start: true,
+      omdbID: "tt0",
+      movieIds:[],
+      title: ""
     };
   },
+  methods: {
+    getmovie() {
+      for (let i = 0; i < 4; i++) {
+        this.omdbID = "tt0";
+        this.title = "";
+        console.log("Entered function movieID");
+        for (var y = 0; y < 6; y++) {
+            // This will loop 6 times
+            let number = Math.floor(Math.random() * 10);
+            let num = number.toString();
+            this.omdbID = this.omdbID + num;
+            console.log(this.omdbID);
+        }
 
-  methods: {}
+        axios.get(baseurl + "&i=" + this.omdbID).then(response => {
+            console.log("here now");
+            console.log(response.data.Title);
+            this.title = response.data.Title;
+            this.movieIds.push(this.title)
+        })
+          
+      }
+        console.log(this.movieIds)
+    },
+  }
 });
 </script>
 
