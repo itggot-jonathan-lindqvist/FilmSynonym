2a1dbeb0377d049509056087943d82c9bf39b96e
eriknord20 <eriknord@PostNord.local>
2018-10-09 14:48:29 +0200

    not ready to publish

--- film/src/views/quiz.vue
+++ film/src/views/quiz.vue
@@ -5,10 +5,14 @@
             .btn-primary.btn-lg.btn.btn-block(@click="start=false, getmovie()" type="button") START
         div(v-else)
             ul.list-group.shadow-lg.p-3.mb-5.bg-white.rounded()
-                li.list-group-item(v-for="movie in movieIds") {{movie}}
+                //- div(v-if="rightAnswer =")                
+                li.list-group-item.list-group.list-group-item-action.blockquote(v-for="(movie,index) in movieIds") {{movie}}
             .plot.card.shadow-lg.bg-white.rounded.p-3.m-5
-                .card-body
-                    p.card-text.text-center.lead Hello ma friend
+                .card-body                                         
+                    p.card-text.text-center.lead {{Plot}}
+            .col-m-12.text-center          
+                .btn.btn-primary.btn-lg.center-block.btn-block.w-25.p-4.float-right.mr-5(disabled = true) NEXT
+            
 
 
 </template>
@@ -16,9 +20,10 @@
 import Vue from "vue";
 import axios from "axios";
 import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
 
 Vue.use(BootstrapVue);
+import "bootstrap/dist/css/bootstrap.css";
+import movieVue from "@/views/movie.vue";
 let key = "57b31362",
   baseurl = "https://www.omdbapi.com/?apikey=" + key;
 export default Vue.extend({
@@ -26,34 +31,48 @@ export default Vue.extend({
     return {
       start: true,
       omdbID: "tt0",
-      movieIds:[],
-      title: ""
+      movieIds: [],
+      rightAnswer: null,
+      title: "",
+      Plot: ""
     };
   },
   methods: {
     getmovie() {
       for (let i = 0; i < 4; i++) {
-        this.omdbID = "tt0";
-        this.title = "";
-        console.log("Entered function movieID");
-        for (var y = 0; y < 6; y++) {
-            // This will loop 6 times
-            let number = Math.floor(Math.random() * 10);
-            let num = number.toString();
-            this.omdbID = this.omdbID + num;
-            console.log(this.omdbID);
+        this.omdbID = "tt0"
+        this.title = ""
+        // console.log("Entered function movieID");
+        for (let y = 0; y < 6; y++) {
+          // This will loop 6 times
+          let number = Math.floor(Math.random() * 10)
+          let num = number.toString()
+          this.omdbID = this.omdbID + num
+          // console.log(this.omdbID);
         }
 
         axios.get(baseurl + "&i=" + this.omdbID).then(response => {
-            console.log("here now");
-            console.log(response.data.Title);
-            this.title = response.data.Title;
-            this.movieIds.push(this.title)
-        })
-          
+          // console.log("here now");
+          // console.log(response.data.Title);
+          this.title = response.data.Title
+          this.movieIds.push(this.title) // fungerar men står fel
+        });
       }
-        console.log(this.movieIds)
+      console.log(this.movieIds)
+      this.getPlot()
     },
+    getPlot() {
+      let rand = Math.floor(Math.random() * 3)
+      console.log(rand)
+      console.log(this.movieIds)
+      axios.get(baseurl + "&t=" + this.movieIds[rand]).then(response => {
+        console.log("test")
+        console.log(this.movieIds[rand])
+        console.log("test")
+        this.rightAnswer = this.movieIds[rand]
+        this.Plot = response.data.Plot
+      });
+    }
   }
 });
 </script>
@@ -70,13 +89,16 @@ export default Vue.extend({
     //     width: 100%
     //     heigth: 30vh
     //     margin: auto
-    // #btn
+    //  #btn
     //     background: #456
     //     padding: 10px
-    //     margin: auto
+    //    margin: auto
 
     // div
     //     width: 100%
+
+
+
 
         
 </style>
