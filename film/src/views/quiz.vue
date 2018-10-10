ace32ffcb2abd1d0e0b84163fb615e369cde609a
eriknord20 <eriknord@PostNord.local>
2018-10-10 15:16:04 +0200

    not ready

--- film/src/views/quiz.vue
+++ film/src/views/quiz.vue
@@ -5,13 +5,14 @@
             .btn-primary.btn-lg.btn.btn-block(@click="start=false, getmovie()" type="button") START
         div(v-else)
             ul.list-group.shadow-lg.p-3.mb-5.bg-white.rounded()
-                //- div(v-if="rightAnswer =")                
-                li.list-group-item.list-group.list-group-item-action.blockquote(v-for="(movie,index) in movieIds") {{movie}}
+                //- div(v-if="rightAnswer =")     
+                           
+                .btn.btn-group-vertical.blockquote.list-group-item-action.active(v-for="(movie,index) in movieIds" ) {{movie}}
             .plot.card.shadow-lg.bg-white.rounded.p-3.m-5
                 .card-body                                         
                     p.card-text.text-center.lead {{Plot}}
             .col-m-12.text-center          
-                .btn.btn-primary.btn-lg.center-block.btn-block.w-25.p-4.float-right.mr-5.disabled(role="button" aria-disabled="true") NEXT
+                .btn.btn-primary.btn-lg.center-block.btn-block.w-25.p-4.float-right.mr-5  NEXT
             
 
 
@@ -32,46 +33,84 @@ export default Vue.extend({
       start: true,
       omdbID: "tt0",
       movieIds: [],
-      rightAnswer: null,
+      rightAnswer: "",
       title: "",
-      Plot: ""
+      Plot: "",
+      // show: false,
     };
   },
+
+  // computed: {
+  //   isDisabled: function() {
+  //     return !this.show
+  // }
+  // },
+
   methods: {
     getmovie() {
-      for (let i = 0; i < 4; i++) {
-        this.omdbID = "tt0"
-        this.title = ""
-        // console.log("Entered function movieID");
-        for (let y = 0; y < 6; y++) {
-          // This will loop 6 times
-          let number = Math.floor(Math.random() * 10)
-          let num = number.toString()
-          this.omdbID = this.omdbID + num
-          // console.log(this.omdbID);
-        }
+      let titles = new Promise((resolve, reject) => {
+        for (let i = 0; i < 4; i++) {
+          this.omdbID = "tt0";
+          this.title = "";
+          // console.log("Entered function movieID");
+          for (let y = 0; y < 6; y++) {
+            // This will loop 6 times
+            let number = Math.floor(Math.random() * 10);
+            let num = number.toString();
+            this.omdbID = this.omdbID + num;
+            // console.log(this.omdbID);
+          }
 
-        axios.get(baseurl + "&i=" + this.omdbID).then(response => {
-          // console.log("here now");
-          // console.log(response.data.Title);
-          this.title = response.data.Title
-          this.movieIds.push(this.title) // fungerar men står fel
-        });
-      }
-      console.log(this.movieIds)
-      this.getPlot()
+          axios.get(baseurl + "&i=" + this.omdbID).then(response => {
+            // console.log("here now");
+            // console.log(response.data.Title);
+            this.movieIds.push(response.data.Title)
+             // fungerar men står fel
+          });
+        }
+        console.log(this.movieIds)
+        console.log()
+        resolve(this.movieIds)
+      })
+      titles.then(response => {
+        let rand = Math.floor(Math.random() * 3);
+        console.log(rand);
+        console.log(this.movieIds.length);
+        console.log()
+        for (let index = 0; index < this.movieIds.length; index++) {
+          console.log(this.movieIds[index]);
+          
+        }
+        
+        console.log(rand)
+        // axios.get(baseurl + "&t=" + this.movieIds[rand]).then(response => {
+        //   console.log("test")
+        //   console.log(this.movieIds[rand])
+        //   console.log("test")
+        //   this.rightAnswer = this.movieIds[rand]
+        //   this.Plot = response.data.Plot
+        // })
+      });
     },
     getPlot() {
-      let rand = Math.floor(Math.random() * 3)
-      console.log(rand)
-      console.log(this.movieIds)
-      console.log(this.movieIds.length)
-      axios.get(baseurl + "&t=" + this.movieIds[rand]).then(response => {
-        console.log("test")
-        console.log(this.movieIds[rand])
-        console.log("test")
-        this.rightAnswer = this.movieIds[rand]
-        this.Plot = response.data.Plot
+      Promise.all(this.movieIds).then(response =>{
+        let rand = Math.floor(Math.random() * 3);
+        console.log(rand);
+        console.log(response)
+
+        let eArr = this.movieIds[Symbol.iterator]();
+        console.log(eArr.next().value);
+        console.log(response)
+        console.log(this.movieIds)
+        console.log(response)
+        console.log(rand)
+        // axios.get(baseurl + "&t=" + this.movieIds[rand]).then(response => {
+        //   console.log("test")
+        //   console.log(this.movieIds[rand])
+        //   console.log("test")
+        //   this.rightAnswer = this.movieIds[rand]
+        //   this.Plot = response.data.Plot
+        // })
       });
     }
   }
