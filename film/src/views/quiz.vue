54ee1d3f0137c449c513e88a65ab6fd260c6ef53
eriknord20 <eriknord@PostNord.local>
2018-10-11 12:16:41 +0200

    work inprogres quiz

--- film/src/views/quiz.vue
+++ film/src/views/quiz.vue
@@ -38,11 +38,6 @@ export default Vue.extend({
       rightAnswer: "",
       title: "",
       Plot: "",
-<<<<<<< HEAD
-      // show: false,
- 
-=======
->>>>>>> a1a98ead800872f20d6128433c042cb7c3ba4825
     };
   },
 
@@ -74,7 +69,30 @@ export default Vue.extend({
 
       console.log(getMovie(this.movieIds[0]))
 
-    }
+    },
+    
+    // smoothScroll(target:any){
+    //     let scrollContainer = target;
+    //     do { //find scroll container
+    //         scrollContainer = scrollContainer.parentNode;
+    //         if (!scrollContainer) return;
+    //         scrollContainer.scrollTop += 1;
+    //     } while (scrollContainer.scrollTop == 0);
+        
+    //     let targetY = 0;
+    //     do { //find the top of target relatively to the container
+    //         if (target == scrollContainer) break;
+    //         targetY += target.offsetTop;
+    //     } while (target = target.offsetParent);
+        
+    //     let scroll = function(c, a, b, i) {
+    //         i++; if (i > 30) return;
+    //         c.scrollTop = a + (b - a) / 30 * i;
+    //         setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    //     }
+
+
+    // }
   }
 })
 
